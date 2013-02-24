719030783123786523341f35306d5452a2d44024
Matt Howard <matrixmickey@gmail.com>
2013-02-24 16:45:48 -0500

    Basic IntelliJ setup

---
+++ Java1.7.0_15Code/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mickey
+ * Date: 2/24/13
+ * Time: 4:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Pumping Lemma");
+    }
+}
