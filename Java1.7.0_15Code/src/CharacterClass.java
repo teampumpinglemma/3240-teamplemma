acd6fcd84ed6b113e940caee4cbed96eb48ea32e
Matt Howard <matrixmickey@gmail.com>
2013-04-08 01:53:44 -0400

    Now it builds the character classes exactly as described in the Character Classes section of the Project1-Regex document. I think I had implied earlier that this was already done, but it was not. All that is left now is building the NFAs for the token regexes, converting them to DFA, and implementing the table walker.

---
+++ Java1.7.0_15Code/src/CharacterClass.java
@@ -0,0 +1,55 @@
+/**
+ * The CharacterClass class has a boolean array for keeping track of which ASCII printable characters are included in the class,
+ * and has methods for easily manipulating the array in useful ways
+ *
+ * Created with IntelliJ IDEA.
+ * User: Mickey
+ * Date: 4/7/13
+ * Time: 9:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CharacterClass {
+    boolean[] accepted;
+    String name;
+    public CharacterClass(String name) {
+        this.name = name;
+        // 95 is the number of ASCII printable characters
+        accepted = new boolean[95];
+    }
+
+    /**
+     * Adds a single character to the character class
+     * @param character :the ASCII value of the character to add
+     */
+    public void setToTrue(int character) {
+        accepted[character - 32] = true;
+    }
+
+    /**
+     * Adds a range of characters to the character class
+     * @param from :the ASCII value of the first character in the range to add
+     * @param to :the ASCII value of the last character in the range to add
+     */
+    public void setToTrue(int from, int to) {
+        if (from > to) {
+            System.out.println("Empty range found while building a character class");
+            System.exit(0);
+        }
+        for (int i = from; i <= to; i++) {
+            accepted[i - 32] = true;
+        }
+    }
+
+    /**
+     * Adds all the characters of "in" to the character class, except for those in "not"
+     * @param not :the characters in "in" to exclude
+     * @param in :the characters to add
+     */
+    public void notBlankInBlank(CharacterClass not, CharacterClass in) {
+        for (int i = 0; i < accepted.length; i++) {
+            if (!not.accepted[i]) {
+                accepted[i] = in.accepted[i];
+            }
+        }
+    }
+}
--- Java1.7.0_15Code/src/Main.java
+++ Java1.7.0_15Code/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 
 /**
  * The Main class runs everything
@@ -11,13 +12,14 @@ import java.io.File;
  */
 public class Main {
     /**
-     * The main method... creates a RegexParser, and then parses the Spec file
+     * The main method... creates a RegexParser, and then parses the Spec file, and then builds the character classes
      *
      * @param args :pass in the Spec file... to do this in IntelliJ, go to Run -> Edit Configurations... and then put the path to the file in "Program arguments"
      */
     public static void main(String[] args) {
         RegexParser regexParser = new RegexParser(new File(args[0]));
         regexParser.parse();
+
         System.out.println("Char Classes");
         for (int i = 0; i < regexParser.specReader.defined.size(); i++) {
             System.out.println(regexParser.specReader.defined.get(i).name);
@@ -33,6 +35,102 @@ public class Main {
             System.out.print("\t");
             for (int j = 0; j < regexParser.specReader.tokens.get(i).tokens.size(); j++) {
                 System.out.print(regexParser.specReader.tokens.get(i).tokens.get(j).token.toString() + " " + regexParser.specReader.tokens.get(i).tokens.get(j).characters + "   ");
+            }
+            System.out.println();
+        }
+
+        // Stores the built character classes
+        ArrayList<CharacterClass> characterClasses = new ArrayList<CharacterClass>();
+        // For every character class that was parsed earlier
+        for (int i = 0; i < regexParser.specReader.defined.size(); i++) {
+            Definition definition = regexParser.specReader.defined.get(i);
+            // Set up a new character class
+            characterClasses.add(new CharacterClass(definition.name));
+            // All character classes should start with a SQUARE_OPEN
+            if (definition.tokens.get(0).token == SpecReader.terminal.SQUARE_OPEN) {
+                // If it's an exclude set
+                if (definition.tokens.get(1).token == SpecReader.terminal.CARROT) {
+                    CharacterClass not = new CharacterClass("");
+                    // The current token (starting with the one after the carrot)
+                    int j = 2;
+                    // While still inside square brackets
+                    while (definition.tokens.get(j).token != SpecReader.terminal.SQUARE_CLOSE) {
+                        // If the next token is a dash, then a range of characters is being defined
+                        if (definition.tokens.get(j + 1).token == SpecReader.terminal.DASH) {
+                            not.setToTrue(definition.tokens.get(j).characters.charAt(definition.tokens.get(j).characters.length()- 1), definition.tokens.get(j + 2).characters.charAt(definition.tokens.get(j + 2).characters.length() - 1));
+                            j += 3;
+                        }
+                        // Else a single character is being defined
+                        else {
+                            not.setToTrue(definition.tokens.get(j).characters.charAt(definition.tokens.get(j).characters.length() - 1));
+                            j += 1;
+                        }
+                    }
+                    j += 2;
+                    CharacterClass in = null;
+                    // If the character class following the IN token is a previously defined character class
+                    if (definition.tokens.get(j).token == SpecReader.terminal.DEFINED) {
+                        // Find which one it is
+                        for (int k = 0; k < characterClasses.size() - 1; k++) {
+                            if (characterClasses.get(k).name.equals(definition.tokens.get(j).characters)) {
+                                in = characterClasses.get(k);
+                                break;
+                            }
+                        }
+                    }
+                    // Else it's a another set of square brackets
+                    else {
+                        in = new CharacterClass("");
+                        j += 1;
+                        // While still inside square brackets
+                        while (definition.tokens.get(j).token != SpecReader.terminal.SQUARE_CLOSE) {
+                            // If the next token is a dash, then a range of characters is being defined
+                            if (definition.tokens.get(j + 1).token == SpecReader.terminal.DASH) {
+                                in.setToTrue(definition.tokens.get(j).characters.charAt(definition.tokens.get(j).characters.length()- 1), definition.tokens.get(j + 2).characters.charAt(definition.tokens.get(j + 2).characters.length() - 1));
+                                j += 3;
+                            }
+                            // Else a single character is being defined
+                            else {
+                                in.setToTrue(definition.tokens.get(j).characters.charAt(definition.tokens.get(j).characters.length() - 1));
+                                j += 1;
+                            }
+                        }
+                    }
+                    characterClasses.get(i).notBlankInBlank(not, in);
+                }
+                // Else it's a normal set of square brackets with characters to include
+                else {
+                    // The current token (starting with the one after the SQUARE_OPEN)
+                    int j = 1;
+                    // While still inside square brackets
+                    while (definition.tokens.get(j).token != SpecReader.terminal.SQUARE_CLOSE) {
+                        // If the next token is a dash, then a range of characters is being defined
+                        if (definition.tokens.get(j + 1).token == SpecReader.terminal.DASH) {
+                            characterClasses.get(i).setToTrue(definition.tokens.get(j).characters.charAt(definition.tokens.get(j).characters.length()- 1), definition.tokens.get(j + 2).characters.charAt(definition.tokens.get(j + 2).characters.length() - 1));
+                            j += 3;
+                        }
+                        // Else a single character is being defined
+                        else {
+                            characterClasses.get(i).setToTrue(definition.tokens.get(j).characters.charAt(definition.tokens.get(j).characters.length() - 1));
+                            j += 1;
+                        }
+                    }
+                }
+            }
+            // Else throw an error
+            else {
+                System.out.println("Not a defined class");
+                System.exit(0);
+            }
+        }
+        System.out.println("\nBuilt Character Classes");
+        for (int i = 0; i < characterClasses.size(); i++) {
+            System.out.println(characterClasses.get(i).name);
+            boolean[] accepted = characterClasses.get(i).accepted;
+            for (int j = 0; j < accepted.length; j++) {
+                if (accepted[j]) {
+                    System.out.print(" " + (char)(j + 32));
+                }
             }
             System.out.println();
         }
