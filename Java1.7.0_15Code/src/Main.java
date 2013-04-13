9e1bd67ee0fbfb235154cd11d766ff7ab915540c
Matt Howard <matrixmickey@gmail.com>
2013-04-12 23:03:42 -0400

    Fully commented code.  Writes a table output file as well as a token output file.

--- Java1.7.0_15Code/src/Main.java
+++ Java1.7.0_15Code/src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 /**
  * The Main class runs everything
@@ -13,15 +15,44 @@ public class Main {
     /**
      * The main method... creates a RegexParser, then parses and builds the NFAs for the Character Classes, then parses and builds the NFAs for the Tokens, then combines the Token NFAs and builds the DFA Table, and then finally scans the input file for tokens and writes them to the output file
      *
-     * @param args :args[0] pass in the absolute path to the Spec file, args[1] pass in the absolute path to the Input file, args[2] pass in the absolute path to the Output file (file does not have to exist yet)
+     * @param args :args[0] pass in the absolute path to the Spec file, args[1] pass in the absolute path to the Input file, args[2] pass in the absolute path to the TableOutput file, args[3] pass in the absolute path to the TokenOutput file (file does not have to exist yet)
      */
     public static void main(String[] args) {
         RegexParser regexParser = new RegexParser(new File(args[0]));
         regexParser.parseCharClasses();
         regexParser.parseTokens();
         regexParser.buildDFATable();
-        new TableWalker(regexParser.dfaTable, new File(args[1]), new File(args[2]));
-        System.out.println("Success! Check your output file!");
+        // write DFA Table to file
+        try {
+            BufferedWriter tableWriter = new BufferedWriter(new FileWriter(new File(args[2])));
+            for (int i = 0; i < regexParser.dfaTable.tableRows.size(); i++) {
+                tableWriter.write("State" + i);
+                tableWriter.newLine();
+                for (int j = 0; j < 95; j++) {
+                    //if (regexParser.dfaTable.tableRows.get(i).nextStates[j] != 1) {
+                        tableWriter.write("   " + (char)(j + 32) + " " + regexParser.dfaTable.tableRows.get(i).nextStates[j]);
+                    //}
+                }
+                tableWriter.newLine();
+            }
+            tableWriter.newLine();
+            tableWriter.write("Accept States");
+            tableWriter.newLine();
+            for (int j = 0; j < regexParser.dfaTable.tableRows.size(); j++) {
+                for (int i = 0; i < regexParser.dfaTable.nfa.acceptStates.size(); i++) {
+                    if (regexParser.dfaTable.tableRows.get(j).nfaStates.contains(regexParser.dfaTable.nfa.acceptStates.get(i))) {
+                        tableWriter.write(" " + j);
+                    }
+                }
+            }
+            tableWriter.close();
+        }
+        catch (Exception e) {
+            System.out.println("Could not write to table file");
+            System.exit(0);
+        }
+        new TableWalker(regexParser.dfaTable, new File(args[1]), new File(args[3]));
+        System.out.println("Success! Check your output files!");
         /*
         System.out.println("Char Classes");
         for (int i = 0; i < regexParser.specReader.defined.size(); i++) {
